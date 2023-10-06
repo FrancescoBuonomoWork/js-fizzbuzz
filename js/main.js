f62af267dde307574007ad5f27f3064ae1aca137
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-06 16:27:20 +0200

    add some style element

--- js/main.js
+++ js/main.js
@@ -15,26 +15,33 @@ for (i = 1; i <= 100; i++){
     // const divisore5 = n % 5;
     // console.log("divisore 3", divisore3);
     
+    const boxDOMElement = document.createElement('div');
+    console.log(boxDOMElement);
+    boxDOMElement.classList.add('box');
+    boxDOMElement.innerHTML = n;
+
     if (n % 3 === 0 && n % 5 === 0) {
         n = "fizz buzz";
+        boxDOMElement.classList.add('fizzbuzz');
+
         // console.log("m-3-5",n);
     } else if (n % 3 === 0) {
         n = "fizz";
+        boxDOMElement.classList.add('fizz');
         // console.log("questo n è multiplo di 3", n);
         // console.log(n);
     } else if (n % 5 === 0) {
         n = "buzz";
+        boxDOMElement.classList.add('buzz');
         // console.log(n);
     } 
     // soluzione main esercizio 
     console.log(n);
 
     // bonus 1
-    const boxDOMElement = document.createElement('div');
-    console.log(boxDOMElement);
-    boxDOMElement.classList.add('box');
-    boxDOMElement.innerHTML = n;
+
     gridDOMElement.append(boxDOMElement)
+
 
     
 }
